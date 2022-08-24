a19acc1cd7a65d7d3f6e16711b041eaad71dd678
izzatRushaidhi <izzatRushaidhi@gmail.com>
2022-08-24 15:15:19 +0800

    fix

--- SWTest.ts
+++ SWTest.ts
@@ -1,48 +1,55 @@
-import * as fs from "fs"
+import * as fs from "fs";
+import axios from "axios";
 
 const getSWPeople = async (url: string, results: any[]) => {
-    const res: any = await fetch(url, { "method": "GET" })
-        .then(v => {
-            return v.json()
-        })
-        .then(async reslt => {
-            if (reslt.next) {
-                await getSWPeople(reslt.next, results)
-            }
+  const res: any = await axios
+    .get(url)
+    .then((v) => {
+      return v.data;
+    })
+    .then(async (reslt) => {
+      if (reslt.next) {
+        await getSWPeople(reslt.next, results);
+      }
 
-            return reslt.results;
-        })
+      return reslt.results;
+    });
 
-    results.push(res)
-    return results;
-}
+  results.push(res);
+  return results;
+};
 
 const restructureArr = (arr, gender: string) => {
-    return arr?.filter(v => v?.gender === gender).map(el => { return { name: el.name, height: el.height } }).sort((a, b) => parseFloat(a.height) - parseFloat(b.height))
-}
+  return arr
+    ?.filter((v) => v?.gender === gender)
+    .map((el) => {
+      return { name: el.name, height: el.height };
+    })
+    .sort((a, b) => parseFloat(a.height) - parseFloat(b.height));
+};
 
 const main = async () => {
-    let curr: any = []
-    let data: any = await getSWPeople("https://swapi.dev/api/people/", curr);
+  let curr: any = [];
+  let data: any = await getSWPeople("https://swapi.dev/api/people/", curr);
 
-    let final: any = {
-        female: [],
-        male: [],
+  let final: any = {
+    female: [],
+    male: [],
+  };
+
+  let flatArr = data.flatMap((v) => v);
+
+  final.female = restructureArr(flatArr, "female");
+  final.male = restructureArr(flatArr, "male");
+
+  let finalData = Object.keys(final).map((v) => {
+    return {
+      gender: v,
+      characters: final[v],
     };
+  });
 
-    let flatArr = data.flatMap(v => v)
-
-    final.female = restructureArr(flatArr, "female")
-    final.male = restructureArr(flatArr, "male")
-
-    let finalData = Object.keys(final).map(v => {
-        return {
-            gender: v,
-            characters: final[v]
-        }
-    })
-
-    fs.writeFile("output.json", JSON.stringify(finalData), () => { })
-}
+  fs.writeFile("output.json", JSON.stringify(finalData), () => {});
+};
 
 main();
